b4531b27ef280320f4191e2f661956b3b7e123c2
Dylan Sutton <dylanlsutton@gmail.com>
2021-01-12 23:55:59 -0500

    Add more sensible controls.

--- crane.gd
+++ crane.gd
@@ -5,24 +5,24 @@ var ball_vertical_speed = 0.04;
 func _physics_process(delta):
 	var limit;
 	var node;
-	if(Input.is_action_pressed("ui_right")):
+	if(Input.is_action_pressed("rotate_right")):
 		get_node("crane_origin").rotate_y(-0.2*delta);
-	elif(Input.is_action_pressed("ui_left")):
+	elif(Input.is_action_pressed("rotate_left")):
 		get_node("crane_origin").rotate_y(0.2*delta)
-	if(Input.is_action_pressed("ui_down")):
+	if(Input.is_action_pressed("backward")):
 		node = get_node("crane_origin/chain_anchor");
 		if(node.transform.origin.x > 2):
 			node.translate(Vector3(-0.05,0,0));
-	elif(Input.is_action_pressed("ui_up")):
+	elif(Input.is_action_pressed("forward")):
 		node = get_node("crane_origin/chain_anchor");
 		if(node.transform.origin.x < 12):
 			node.translate(Vector3(0.05,0,0));
-	if(Input.is_action_pressed("ui_page_up")):
+	if(Input.is_action_pressed("raise")):
 		node = get_node("chain_placeholder/SliderJoint");
 		limit = node.get_param(SliderJoint.PARAM_LINEAR_LIMIT_LOWER);
 		if(limit < 8.9):
 			node.set_param(SliderJoint.PARAM_LINEAR_LIMIT_LOWER, limit + ball_vertical_speed);
-	elif(Input.is_action_pressed("ui_page_down")):
+	elif(Input.is_action_pressed("lower")):
 		node = get_node("chain_placeholder/SliderJoint");
 		limit = node.get_param(SliderJoint.PARAM_LINEAR_LIMIT_LOWER);
 		if(limit > -1):
