1741a628d882bc527533a263c120310db99c0fe9
Dylan Sutton <dylanlsutton@gmail.com>
2021-01-17 17:00:22 -0500

    Add second target and reorganize node hierarchy.

--- level1.gd
+++ level1.gd
@@ -4,7 +4,10 @@ var score;
 
 func _process(delta):
 	score = 0;
-	for i in $target1.get_children():
-		if(!$target1area.overlaps_body(i.get_node("RigidBody"))):
+	for i in $target1.get_node("wall").get_children():
+		if(!$target1.get_node("area").overlaps_body(i.get_node("RigidBody"))):
+			score+=1;
+	for i in $target2.get_node("wall").get_children():
+		if(!$target2.get_node("area").overlaps_body(i.get_node("RigidBody"))):
 			score+=1;
 	$score.text = str(score);
