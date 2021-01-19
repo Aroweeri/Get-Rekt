a3eb6e69757f55910b072774a8d16b0d3edc1ea2
Dylan Sutton <dylanlsutton@gmail.com>
2021-01-19 11:37:44 -0500

    Add quit with escape and fullscreen.

--- level1.gd
+++ level1.gd
@@ -3,6 +3,8 @@ extends Spatial
 var score;
 
 func _process(delta):
+	if(Input.is_action_just_pressed("quit")):
+		get_tree().quit();
 	score = 0;
 	for i in $target1.get_node("wall").get_children():
 		if(!$target1.get_node("area").overlaps_body(i.get_node("RigidBody"))):
